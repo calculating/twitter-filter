eafb64bc0a0fe1fde0a88b2ab091296b354f8fa0
Ulisse Mini <ulisse.mini@gmail.com>
2023-08-03 18:26:04 +0900

    Shitty login with twitter poc

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,105 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import fastifyCookie from "@fastify/cookie";
+import fastifySession from "@fastify/session";
+import fetch from "node-fetch";
+import OAuth from "oauth";
 
 const fastify = Fastify({ logger: true });
+
 sqlite3.verbose();
+
+// @fastify/env wasn't working well lol
+fastify.config = process.env;
 
 fastify.register(cors, {
   origin: "https://twitter.com",
   methods: ["GET", "POST"], // replace with the methods you want to allow
 });
 
+fastify.register(fastifyCookie);
+fastify.register(fastifySession, {
+  secret: fastify.config.SESSION_SECRET,
+  cookie: { secure: false }, // TODO: Why??
+});
+
+const oa = new OAuth.OAuth(
+  "https://api.twitter.com/oauth/request_token",
+  "https://api.twitter.com/oauth/access_token",
+  fastify.config.TWITTER_CONSUMER_KEY,
+  fastify.config.TWITTER_CONSUMER_SECRET,
+  "1.0A",
+  "http://localhost:3000/callback",
+  "HMAC-SHA1"
+);
+
 let db;
+
+fastify.get("/login/twitter", function handler(req, reply) {
+  oa.getOAuthRequestToken((error, oauth_token, oauth_token_secret, _res) => {
+    if (error) {
+      fastify.log.error(error);
+      return reply.send("Authentication Failed!");
+    } else {
+      req.session.oauth = {
+        token: oauth_token,
+        token_secret: oauth_token_secret,
+      };
+      fastify.log.info(req.session.oauth);
+      return reply.redirect(
+        "https://twitter.com/oauth/authenticate?oauth_token=" + oauth_token
+      );
+    }
+  });
+});
+
+fastify.get("/callback", function (req, reply) {
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    const oauth = req.session.oauth;
+
+    // TODO: Promisfy and async this.
+    oa.getOAuthAccessToken(
+      oauth.token,
+      oauth.token_secret,
+      oauth.verifier,
+      (error, oauth_access_token, oauth_access_token_secret, results) => {
+        if (error) {
+          fastify.log.error(error);
+          return reply.send("Authentication Failure!");
+        } else {
+          req.session.oauth.access_token = oauth_access_token;
+          req.session.oauth.access_token_secret = oauth_access_token_secret;
+          fastify.log.info(results, req.session.oauth);
+
+          // you might want to start using the Access Token to make authenticated requests to the user's Twitter account at this point
+          // get user profile info as a test
+
+          oa.get(
+            "https://api.twitter.com/1.1/account/verify_credentials.json",
+            oauth_access_token,
+            oauth_access_token_secret,
+            (error, data, _res) => {
+              if (error) {
+                fastify.log.error(error);
+                return reply.send("Authentication Failure!");
+              } else {
+                const parsedData = JSON.parse(data);
+                fastify.log.info(parsedData);
+                return reply.send(
+                  `<p>You are signed in: ${parsedData.screen_name}</p>`
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+  } else {
+    return reply.send("you're not supposed to be here.");
+  }
+});
 
 fastify.get("/api/tweets", async function handler(_request, reply) {
   try {
