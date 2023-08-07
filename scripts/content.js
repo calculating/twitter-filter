f7c8d6b7bad76515c44654d2a5ff8ed471dbcd66
Laura Gao <gaolauro@gmail.com>
2023-08-07 13:09:28 +0800

    add reset button

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,11 @@
 const SYSTEM_PROMPT_KEY = "system-prompt";
 const MULTISHOT_PROMPT_KEY = "multishot-prompt";
 
+const DEFAULT_SYSTEM_PROMPT = `Given the following preferences, mark Tweets as either PASS or FILTER, where filtered Tweets will be removed from the user's feed.\n\nPreferences:\n- like: big news about physics\n- dislike: non-substantive content, opinions/questions\n- like: posts sharing ai research papers\n- dislike: nearly anything cryptocurrency related, unless it's particularly interesting from a mathematics or cryptography perspective\n- err on the side of passing posts that don't have enough context\n- filter posts that are social in essence, about someone's personal life\n- in general, if you don't learn anything new about actual things in the world from a tweet, it should be filtered. For example, "this is going to be the most chaotic decade in human history" doesn't actually contain any substative information.\n- No rhetorical question tweets.\n- no promotional tweets\n- nothing that seems like a clickbaity list, i.e. "THESE 5 SIMPLE TRICKS WILL MAKE YOU LITTERALLY MAGIC"`;
+
 function start() {
     if (localStorage.getItem(SYSTEM_PROMPT_KEY) == null) {
-        localStorage.setItem(SYSTEM_PROMPT_KEY, `Given the following preferences, mark Tweets as either PASS or FILTER, where filtered Tweets will be removed from the user's feed.\n\nPreferences:\n- like: big news about physics\n- dislike: non-substantive content, opinions/questions\n- like: posts sharing ai research papers\n- dislike: nearly anything cryptocurrency related, unless it's particularly interesting from a mathematics or cryptography perspective\n- err on the side of passing posts that don't have enough context\n- filter posts that are social in essence, about someone's personal life\n- in general, if you don't learn anything new about actual things in the world from a tweet, it should be filtered. For example, "this is going to be the most chaotic decade in human history" doesn't actually contain any substative information.\n- No rhetorical question tweets.\n- no promotional tweets\n- nothing that seems like a clickbaity list, i.e. "THESE 5 SIMPLE TRICKS WILL MAKE YOU LITTERALLY MAGIC"`);
+        localStorage.setItem(SYSTEM_PROMPT_KEY, DEFAULT_SYSTEM_PROMPT)
     }
 
     if (localStorage.getItem(MULTISHOT_PROMPT_KEY) == null) {
@@ -15,7 +17,7 @@ function start() {
     multishotPrompt = JSON.parse(localStorage.getItem(MULTISHOT_PROMPT_KEY))
 
     IS_LIGHT_MODE = document.head.querySelector("[name~=theme-color][content]").content === "#FFFFFF"
-    IS_LIGHT_MODE = false
+    // IS_LIGHT_MODE = false
     RED = IS_LIGHT_MODE ? "#ff9999" : "#660000"
     YELLOW = IS_LIGHT_MODE ? "#fffdb5" : "#4B4901"
     BLUE = IS_LIGHT_MODE ? "#b5e9ff" : "#00354B"
@@ -53,6 +55,21 @@ function start() {
     const targetNode = document.querySelector("body");
 
     observer.observe(targetNode, config);
+
+    // add reset button
+    const resetButton = document.createElement("button")
+    // maybe have an alert are u sure
+    // this is also why. if the user resets their preferences the preference should still exist on the server for our data collection
+    resetButton.onclick = () => {
+        multishotPrompt = []
+        localStorage.setItem(MULTISHOT_PROMPT_KEY, JSON.stringify(multishotPrompt));
+
+        systemPrompt = DEFAULT_SYSTEM_PROMPT;
+        localStorage.setItem(SYSTEM_PROMPT_KEY, systemPrompt);
+    }
+    resetButton.className = "reset-button"
+    resetButton.innerHTML = "reset all preferences"
+    document.body.appendChild(resetButton)
 
     // check all tweets on page load
     checkall();
